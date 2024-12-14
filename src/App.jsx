055b68e5aedd9a5b46ec48c1d07d8234950e1e41
programer12345anwar <mdanwar40212@gmail.com>
2024-12-14 20:03:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./customer/components/Navigation";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
